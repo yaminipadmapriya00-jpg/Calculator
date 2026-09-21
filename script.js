1f4fa1e6e83198ba173f9062bd7fad7c73fe4cc6
yaminipadmapriya00-jpg <yaminipadmapriya00@gmail.com>
2026-09-21 23:30:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let expression = display.value;
+        expression = expression.replace(/%/g, "/100");
+        display.value = eval(expression);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+
+    const key = event.key;
+
+    if (
+        (key >= "0" && key <= "9") ||
+        key === "+" ||
+        key === "-" ||
+        key === "*" ||
+        key === "/" ||
+        key === "." ||
+        key === "%"
+    ) {
+        appendValue(key);
+    }
+
+    else if (key === "Enter" || key === "=") {
+        calculate();
+    }
+
+    else if (key === "Backspace") {
+        deleteLast();
+    }
+
+    else if (key === "Escape") {
+        clearDisplay();
+    }
+})
